dacb304e9c23f9c526e0d2c8af19e10f6aae8034
Ned Bellavance <ned@nedinthecloud.com>
2020-11-17 16:56:07 -0500

    2020-11-17

---
+++ 2020-11-17/main.tf
@@ -0,0 +1,14 @@
+locals {
+    str1 = "tacos"
+    str2 = "burrittos"
+
+    n1 = 0
+    n2 = 1
+
+    l1 = ["tacos","enchiladas"]
+
+    m1 = {
+        meat = "chicken"
+        cheese = "cheddar jack"
+    }
+}
