39a871da8e396f5215428f0f21cf143399d4ffeb
ned1313 <ned.bellavance@gmail.com>
2021-01-05 15:52:30 -0500

    2021-01-05

---
+++ 2021-01-05/main.tf
@@ -0,0 +1,77 @@
+###########################
+# CONFIGURATION
+###########################
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.40"
+
+    }
+    #azuread = {
+    #  source  = "hashicorp/azuread"
+    #  version = "=0.11.0"
+    #}
+  }
+}
+
+###########################
+# VARIABLES
+###########################
+
+variable "region" {
+  type        = string
+  description = "Region in Azure"
+  default     = "eastus"
+}
+
+variable "prefix" {
+  type        = string
+  description = "prefix for naming"
+  default     = "tacos"
+}
+
+###########################
+# PROVIDERS
+###########################
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {}
+
+###########################
+# DATA SOURCES
+###########################
+
+locals {
+  name = "${var.prefix}-cli"
+}
+
+###########################
+# RESOURCES
+###########################
+
+resource "azurerm_resource_group" "vnet" {
+  name     = local.name
+  location = var.region
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  version             = "3.1.1"
+  resource_group_name = azurerm_resource_group.vnet.name
+  vnet_name           = local.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.vnet]
+}
