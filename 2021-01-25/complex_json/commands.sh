a41ef6ba376ba2c339a2729bb0ad73b2aee0aa28
ned1313 <ned.bellavance@gmail.com>
2021-01-26 16:54:40 -0500

    fix last command

--- 2021-01-25/complex_json/commands.sh
+++ 2021-01-25/complex_json/commands.sh
@@ -26,9 +26,9 @@ local.vnet_json_data.k8s-vnet.Subnets
 [ for subnet in local.vnet_json_data.k8s-vnet.Subnets : { name = subnet.Name, cidr_address = cidrsubnet("10.12.0.0/16", 8, subnet.Priority) } ]
 
 # Awesome! Now what we want is to do a nested for loop and include the vnet Name
-[ for network_key, network in local.vnet_json_data : [ for subnet in network.Subnets : { name = subnet.Name, cidr_address = cidrsubnet("10.12.0.0/16", 8, subnet.Priority), vnet = network_key } ] if network_key != "hub-vnet" ]
+[ for network_key, network in local.vnet_json_data : [ for subnet in network.Subnets : { name = subnet.Name, cidr_address = cidrsubnet(network.AddressSpace, 8, subnet.Priority), vnet = network_key } ] if network_key != "hub-vnet" ]
 
 # Hoo-boy! Now we're cooking with gas! But we've got a list of lists and that's no good
 # We can use flatten to get a list of maps
-flatten([ for network_key, network in local.vnet_json_data : [ for subnet in network.Subnets : { name = subnet.Name, cidr_address = cidrsubnet("10.12.0.0/16", 8, subnet.Priority), vnet = network_key } ] if network_key != "hub-vnet" ])
+flatten([ for network_key, network in local.vnet_json_data : [ for subnet in network.Subnets : { name = subnet.Name, cidr_address = cidrsubnet(network.AddressSpace, 8, subnet.Priority), vnet = network_key } ] if network_key != "hub-vnet" ])
 
