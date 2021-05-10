bee73fc4dda1cae1284ad34a2ed69b146cc5ce46
ned1313 <ned.bellavance@gmail.com>
2021-05-10 08:49:07 -0400

    add subnet

--- AppsOn/AWS/main.tf
+++ AppsOn/AWS/main.tf
@@ -64,7 +64,7 @@ module "vpc" {
   cidr = "10.0.0.0/16"
   azs = ["us-east-1a","us-east-1b"]
   private_subnets = []
-  public_subnets = ["10.0.0.0/24","10.0.1.0/24"]
+  public_subnets = ["10.0.0.0/24","10.0.1.0/24","10.0.2.0/24"]
   enable_nat_gateway = false
 
 }
