712e7300f78c26390cbd0d848d1cb14555f29fa7
ned1313 <ned.bellavance@gmail.com>
2021-05-10 10:43:53 -0400

    change subnet

--- AppsOn/Azure/main.tf
+++ AppsOn/Azure/main.tf
@@ -67,7 +67,7 @@ module "network" {
   resource_group_name = azurerm_resource_group.vnet.name
   vnet_name           = local.name
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.0.0/24","10.0.1.0/24"]
+  subnet_prefixes     = ["10.0.0.0/24","10.0.2.0/24"]
   subnet_names        = ["subnet1", "subnet2"]
 
   depends_on = [azurerm_resource_group.vnet]
