943da3e2ae2e93988c21511c6984347c8e7464bb
ned1313 <ned.bellavance@gmail.com>
2021-05-23 19:04:56 -0400

    key vault ADO config

---
+++ 2021-05-25-ADO/vnet/main.tf
@@ -0,0 +1,74 @@
+###########################
+# CONFIGURATION
+###########################
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+
+    }
+  }
+
+  backend "azurerm" {
+    
+  }
+}
+
+###########################
+# VARIABLES
+###########################
+
+variable "region" {
+  type        = string
+  description = "Region in Azure"
+  default     = "eastus"
+}
+
+variable "prefix" {
+  type        = string
+  description = "prefix for naming"
+  default     = "tacos"
+}
+
+###########################
+# PROVIDERS
+###########################
+
+provider "azurerm" {
+  features {}
+}
+
+###########################
+# DATA SOURCES
+###########################
+
+locals {
+  name = "${var.prefix}-${random_id.seed.hex}"
+}
+
+###########################
+# RESOURCES
+###########################
+
+resource "random_id" "seed" {
+  byte_length = 4
+}
+
+resource "azurerm_resource_group" "vnet" {
+  name     = local.name
+  location = var.region
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  version             = "3.1.1"
+  resource_group_name = azurerm_resource_group.vnet.name
+  vnet_name           = local.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.0.0/24","10.0.2.0/24","10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2","subnet3"]
+
+  depends_on = [azurerm_resource_group.vnet]
+}
