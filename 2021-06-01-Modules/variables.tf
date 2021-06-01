e2413e4b3ffeef94a3748e7dfb88cea8e2522cbe
ned1313 <ned.bellavance@gmail.com>
2021-06-01 18:43:38 -0400

    2021-06-01

---
+++ 2021-06-01-Modules/variables.tf
@@ -0,0 +1,14 @@
+variable "meat" {
+  type = string
+  default = "chicken"
+}
+
+variable "cheese" {
+  type = string
+  default = "jack"
+}
+
+variable "shell" {
+  type = string
+  default = "corn"
+}
