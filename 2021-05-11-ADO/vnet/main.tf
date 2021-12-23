8e574fd461967038754393818f644761a77a84f8
j7singh <terraform_cicd_devops@outlook.com>
2021-12-23 16:48:06 -0500

    removed subnet

--- 2021-05-11-ADO/vnet/main.tf
+++ 2021-05-11-ADO/vnet/main.tf
@@ -12,7 +12,7 @@ terraform {
   }
 
   backend "azurerm" {
-    
+
   }
 }
 
@@ -37,11 +37,11 @@ variable "prefix" {
 ###########################
 
 provider "azurerm" {
-  ARM_SAS_TOKEN = var.sas_token
-  ARM_CLIENT_ID = var.az_client_id
-  ARM_CLIENT_SECRET = var.az_client_secret
-  ARM_SUBSCRIPTION_ID = var.az_subscription
-  ARM_TENANT_ID = var.az_tenant
+  sas_token       = var.sas_token
+  client_id       = var.az_client_id
+  client_secret   = var.az_client_secret
+  subscription_id = var.az_subscription
+  tennad_id       = var.az_tenant
   features {}
 }
 
@@ -72,38 +72,28 @@ module "network" {
   resource_group_name = azurerm_resource_group.vnet.name
   vnet_name           = local.name
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.0.0/24","10.0.2.0/24","10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2","subnet3"]
+  subnet_prefixes     = ["10.0.0.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
   depends_on = [azurerm_resource_group.vnet]
 }
 
-variable {
-    name         = "sas_token"
-    secret_value = var.sas_token
-    is_secret    = true
-  }
+variable "sas_token" {
 
-  variable {
-    name         = "az_client_id"
-    secret_value = var.az_client_id
-    is_secret    = true
-  }
+}
 
-  variable {
-    name         = "az_client_secret"
-    secret_value = var.az_client_secret
-    is_secret    = true
-  }
+variable "az_client_id" {
 
-  variable {
-    name         = "az_subscription"
-    secret_value = var.az_subscription
-    is_secret    = true
-  }
+}
 
-  variable {
-    name         = "az_tenant"
-    secret_value = var.az_tenant
-    is_secret    = true
-  }
+variable "az_client_secret" {
+
+}
+
+variable "az_subscription" {
+
+}
+
+variable "az_tenant" {
+
+}
