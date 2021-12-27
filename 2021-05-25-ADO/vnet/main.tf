b2957607be46914710fd3cc5157417f5a4f40167
j7singh <terraform_cicd_devops@outlook.com>
2021-12-27 14:43:50 -0500

    updated

--- 2021-05-25-ADO/vnet/main.tf
+++ 2021-05-25-ADO/vnet/main.tf
@@ -6,13 +6,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "~> 2.90.0"
 
     }
   }
 
   backend "azurerm" {
-    
+
   }
 }
 
@@ -23,13 +23,13 @@ terraform {
 variable "region" {
   type        = string
   description = "Region in Azure"
-  default     = "eastus"
+  default     = "canadacentral"
 }
 
 variable "prefix" {
   type        = string
   description = "prefix for naming"
-  default     = "tacos"
+  default     = "test-tf-pipeline"
 }
 
 ###########################
@@ -67,7 +67,7 @@ module "network" {
   resource_group_name = azurerm_resource_group.vnet.name
   vnet_name           = local.name
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.0.0/24","10.0.2.0/24","10.0.3.0/24"]
+  subnet_prefixes     = ["10.0.0.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
   depends_on = [azurerm_resource_group.vnet]
